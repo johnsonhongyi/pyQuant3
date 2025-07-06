9107e527d1598a522c35ffaf7a2228685f94c2b2
johnsonhongyi <johnson.hongyi@gmail.com>
2025-07-06 18:11:32 +0800

    macrun add time

--- stock/macRun.sh
+++ stock/macRun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+startTime=`date +%s`
 cd /Users/Johnson/Documents/Quant/pyQuant3/stock
 echo "sina_Monitor.py"
 python -m nuitka --show-memory --show-progress --follow-import-to=JohnsonUtil,JSONData --remove-output --lto=yes sina_Monitor.py
@@ -27,3 +28,8 @@ sleep 2
 mv chantdxpower.bin dataBarFeed/
 ls -alh dataBarFeed/chantdxpower.*
 # rem start python LineHistogram.py
+#do something here
+endTime=`date +%s`
+sumTime=$(( $endTime - $startTime ))
+minT=`expr $endTime / 60`
+echo "Total run $sumTime seconds min: $minT min"
